c592231857099fbbf3786146c224f017dc8c9be5
rouanthompson <rouan.thompson@gmail.com>
2021-04-03 17:06:43 -0400

    PI constant printed

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -3,6 +3,10 @@ package com.FirstJavaSteps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final float PI = 3.14F;
+        System.out.println(PI);
+        // The keyword Final is used let the compiler know that PI will not change
+        // This is useful for when for when we don't want a variable to change
+        // constants variable names are all caps
     }
 }
